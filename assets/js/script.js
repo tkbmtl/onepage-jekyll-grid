8c6f88458f95435c52e1c8dbee547d79d5e6f638
tkbmtl <e@squaredynamics.com>
2020-12-22 10:19:51 -0500

    burger and JS amelioration and links

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,52 @@ const navSlide = () => {
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li')
 
+    //remove easy-in on window resize
+    window.addEventListener('resize', () => {
+        nav.classList.remove('easy-in');
+        nav.classList.remove('nav-active');
+        burger.classList.remove('toggle');
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+         });
+    });
+
     //toggle nav
     burger.addEventListener('click', () => {
+
+        nav.classList.add('easy-in');
         nav.classList.toggle('nav-active');
 
-            //Animate linkls
+        //Animate linkls
         navLinks.forEach((link, index) => {
-            console.log(link.style.animation)
             if (link.style.animation) {
                 link.style.animation = '';
             } else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
             }
          });
+         //Burger animation
+         burger.classList.toggle('toggle')
     });
+    //check for link click on close everything
+    navLinks.forEach((link) => {
+        link.addEventListener('click', () => {
+            nav.classList.toggle('nav-active');
+            burger.classList.toggle('toggle')
+            navLinks.forEach((link) => {
+                link.style.animation = '';
+            });
+        });
+    });
+        
+
 
 }
 
-navSlide();
+const app = () => {
+    navSlide();
+}
+
+app();
